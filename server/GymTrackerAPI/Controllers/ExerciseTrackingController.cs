0698aa7e1a4c50b09bcbab37f1b205ed012f7810
benyaminahmed <benyaminahmed2@gmail.com>
2024-01-27 15:58:41 +0000

    pushed code

--- server/GymTrackerAPI/Controllers/ExerciseTrackingController.cs
+++ server/GymTrackerAPI/Controllers/ExerciseTrackingController.cs
@@ -54,19 +54,9 @@ namespace GymTrackerAPI.Controllers
 
             return results;
         }
-        /*
+     
         [HttpPost]
-        public async Task<ActionResult<TodoItem>> PostTodoItem(TodoItem todoItem)
-        {
-            _context.TodoItems.Add(todoItem);
-            await _context.SaveChangesAsync();
-
-            //    return CreatedAtAction("GetTodoItem", new { id = todoItem.Id }, todoItem);
-            return CreatedAtAction(nameof(GetTodoItem), new { id = todoItem.Id }, todoItem);
-        }*/
-
-        [HttpPost]
-        public async void CreateExerciseTracking(ExerciseTrackingRequest request)
+        public async Task CreateExerciseTracking(ExerciseTrackingRequest request)
         {
             var et = new ExerciseTracking()
             {
