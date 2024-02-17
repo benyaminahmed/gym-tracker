6f1f03163cc92d671ce6b2df8c02766116f46197
benyaminahmed <benyaminahmed2@gmail.com>
2024-02-17 10:07:09 +0000

    check in to observe errors

--- ui/app/src/main/java/com/example/gymtracker/ui/home/HomeViewModel.kt
+++ ui/app/src/main/java/com/example/gymtracker/ui/home/HomeViewModel.kt
@@ -1,13 +1,22 @@
-package com.example.gymtracker.ui.home
-
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 
+data class Exercise(
+    val exerciseId: String,
+    val exerciseName: String,
+    val unit: String,
+    val createdDate: String
+)
+
 class HomeViewModel : ViewModel() {
 
-    private val _text = MutableLiveData<String>().apply {
-        value = "This is home Fragment"
+    private val _exercises = MutableLiveData<List<Exercise>>().apply {
+        value = listOf(
+            Exercise("3fa85f64-5717-4562-b3fc-2c963f66afa6", "Push-up", "Reps", "2024-02-17T09:49:58.635Z"),
+            Exercise("4fa85f64-5717-4562-b3fc-2c963f66afa7", "Squat", "Reps", "2024-02-17T09:50:58.635Z"),
+            // Add more exercises here
+        )
     }
-    val text: LiveData<String> = _text
+    val exercises: LiveData<List<Exercise>> = _exercises
 }
