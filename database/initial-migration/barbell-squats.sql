1ec5a47b1d8ab0c2074e6cd1d613b242ddb56501
benyaminahmed <benyaminahmed2@gmail.com>
2024-02-20 15:54:35 +0000

    added scripts for squats and leg press

---
+++ database/initial-migration/barbell-squats.sql
@@ -0,0 +1,319 @@
+INSERT INTO public.exercise_tracking (exercise_id, user_id, performance_metric, created_date)
+SELECT 
+    e.exercise_id,
+    u.user_id,
+    25,
+    '2023-01-01'
+FROM
+    public.exercise e, public.user u
+WHERE
+    e.exercise = 'Barbell Squats' AND u.first_name = 'Benyamin';
+
+INSERT INTO public.exercise_tracking (exercise_id, user_id, performance_metric, created_date)
+SELECT 
+    e.exercise_id,
+    u.user_id,
+    25,
+    '2023-01-01'
+FROM
+    public.exercise e, public.user u
+WHERE
+    e.exercise = 'Barbell Squats' AND u.first_name = 'Yusuf';
+
+INSERT INTO public.exercise_tracking (exercise_id, user_id, performance_metric, created_date)
+SELECT 
+    e.exercise_id,
+    u.user_id,
+    25,
+    '2023-01-01'
+FROM
+    public.exercise e, public.user u
+WHERE
+    e.exercise = 'Barbell Squats' AND u.first_name = 'Imran';
+
+INSERT INTO public.exercise_tracking (exercise_id, user_id, performance_metric, created_date)
+SELECT 
+    e.exercise_id,
+    u.user_id,
+    23,
+    '2023-01-15'
+FROM
+    public.exercise e, public.user u
+WHERE
+    e.exercise = 'Barbell Squats' AND u.first_name = 'Benyamin';
+
+INSERT INTO public.exercise_tracking (exercise_id, user_id, performance_metric, created_date)
+SELECT 
+    e.exercise_id,
+    u.user_id,
+    28,
+    '2023-01-15'
+FROM
+    public.exercise e, public.user u
+WHERE
+    e.exercise = 'Barbell Squats' AND u.first_name = 'Yusuf';
+
+INSERT INTO public.exercise_tracking (exercise_id, user_id, performance_metric, created_date)
+SELECT 
+    e.exercise_id,
+    u.user_id,
+    28,
+    '2023-01-15'
+FROM
+    public.exercise e, public.user u
+WHERE
+    e.exercise = 'Barbell Squats' AND u.first_name = 'Imran';
+
+INSERT INTO public.exercise_tracking (exercise_id, user_id, performance_metric, created_date)
+SELECT 
+    e.exercise_id,
+    u.user_id,
+    23,
+    '2023-01-29'
+FROM
+    public.exercise e, public.user u
+WHERE
+    e.exercise = 'Barbell Squats' AND u.first_name = 'Benyamin';
+
+INSERT INTO public.exercise_tracking (exercise_id, user_id, performance_metric, created_date)
+SELECT 
+    e.exercise_id,
+    u.user_id,
+    28,
+    '2023-01-29'
+FROM
+    public.exercise e, public.user u
+WHERE
+    e.exercise = 'Barbell Squats' AND u.first_name = 'Yusuf';
+
+INSERT INTO public.exercise_tracking (exercise_id, user_id, performance_metric, created_date)
+SELECT 
+    e.exercise_id,
+    u.user_id,
+    28,
+    '2023-01-29'
+FROM
+    public.exercise e, public.user u
+WHERE
+    e.exercise = 'Barbell Squats' AND u.first_name = 'Imran';
+
+INSERT INTO public.exercise_tracking (exercise_id, user_id, performance_metric, created_date)
+SELECT 
+    e.exercise_id,
+    u.user_id,
+    23,
+    '2023-02-13'
+FROM
+    public.exercise e, public.user u
+WHERE
+    e.exercise = 'Barbell Squats' AND u.first_name = 'Benyamin';
+
+INSERT INTO public.exercise_tracking (exercise_id, user_id, performance_metric, created_date)
+SELECT 
+    e.exercise_id,
+    u.user_id,
+    28,
+    '2023-02-13'
+FROM
+    public.exercise e, public.user u
+WHERE
+    e.exercise = 'Barbell Squats' AND u.first_name = 'Yusuf';
+
+INSERT INTO public.exercise_tracking (exercise_id, user_id, performance_metric, created_date)
+SELECT 
+    e.exercise_id,
+    u.user_id,
+    28,
+    '2023-02-13'
+FROM
+    public.exercise e, public.user u
+WHERE
+    e.exercise = 'Barbell Squats' AND u.first_name = 'Imran';
+
+INSERT INTO public.exercise_tracking (exercise_id, user_id, performance_metric, created_date)
+SELECT 
+    e.exercise_id,
+    u.user_id,
+    13,
+    '2023-02-27'
+FROM
+    public.exercise e, public.user u
+WHERE
+    e.exercise = 'Barbell Squats' AND u.first_name = 'Benyamin';
+
+INSERT INTO public.exercise_tracking (exercise_id, user_id, performance_metric, created_date)
+SELECT 
+    e.exercise_id,
+    u.user_id,
+    13,
+    '2023-02-27'
+FROM
+    public.exercise e, public.user u
+WHERE
+    e.exercise = 'Barbell Squats' AND u.first_name = 'Imran';
+
+INSERT INTO public.exercise_tracking (exercise_id, user_id, performance_metric, created_date)
+SELECT 
+    e.exercise_id,
+    u.user_id,
+    18,
+    '2023-03-14'
+FROM
+    public.exercise e, public.user u
+WHERE
+    e.exercise = 'Barbell Squats' AND u.first_name = 'Benyamin';
+
+INSERT INTO public.exercise_tracking (exercise_id, user_id, performance_metric, created_date)
+SELECT 
+    e.exercise_id,
+    u.user_id,
+    25.5,
+    '2023-03-14'
+FROM
+    public.exercise e, public.user u
+WHERE
+    e.exercise = 'Barbell Squats' AND u.first_name = 'Yusuf';
+
+INSERT INTO public.exercise_tracking (exercise_id, user_id, performance_metric, created_date)
+SELECT 
+    e.exercise_id,
+    u.user_id,
+    25.5,
+    '2023-03-14'
+FROM
+    public.exercise e, public.user u
+WHERE
+    e.exercise = 'Barbell Squats' AND u.first_name = 'Imran';
+
+INSERT INTO public.exercise_tracking (exercise_id, user_id, performance_metric, created_date)
+SELECT 
+    e.exercise_id,
+    u.user_id,
+    23,
+    '2023-03-28'
+FROM
+    public.exercise e, public.user u
+WHERE
+    e.exercise = 'Barbell Squats' AND u.first_name = 'Benyamin';
+
+INSERT INTO public.exercise_tracking (exercise_id, user_id, performance_metric, created_date)
+SELECT 
+    e.exercise_id,
+    u.user_id,
+    28,
+    '2023-03-28'
+FROM
+    public.exercise e, public.user u
+WHERE
+    e.exercise = 'Barbell Squats' AND u.first_name = 'Imran';
+
+INSERT INTO public.exercise_tracking (exercise_id, user_id, performance_metric, created_date)
+SELECT 
+    e.exercise_id,
+    u.user_id,
+    20.5,
+    '2023-04-11'
+FROM
+    public.exercise e, public.user u
+WHERE
+    e.exercise = 'Barbell Squats' AND u.first_name = 'Benyamin';
+
+INSERT INTO public.exercise_tracking (exercise_id, user_id, performance_metric, created_date)
+SELECT 
+    e.exercise_id,
+    u.user_id,
+    33,
+    '2023-04-11'
+FROM
+    public.exercise e, public.user u
+WHERE
+    e.exercise = 'Barbell Squats' AND u.first_name = 'Yusuf';
+
+INSERT INTO public.exercise_tracking (exercise_id, user_id, performance_metric, created_date)
+SELECT 
+    e.exercise_id,
+    u.user_id,
+    33,
+    '2023-04-11'
+FROM
+    public.exercise e, public.user u
+WHERE
+    e.exercise = 'Barbell Squats' AND u.first_name = 'Imran';
+
+INSERT INTO public.exercise_tracking (exercise_id, user_id, performance_metric, created_date)
+SELECT 
+    e.exercise_id,
+    u.user_id,
+    25.5,
+    '2023-04-26'
+FROM
+    public.exercise e, public.user u
+WHERE
+    e.exercise = 'Barbell Squats' AND u.first_name = 'Benyamin';
+
+INSERT INTO public.exercise_tracking (exercise_id, user_id, performance_metric, created_date)
+SELECT 
+    e.exercise_id,
+    u.user_id,
+    28,
+    '2023-04-26'
+FROM
+    public.exercise e, public.user u
+WHERE
+    e.exercise = 'Barbell Squats' AND u.first_name = 'Imran';
+
+INSERT INTO public.exercise_tracking (exercise_id, user_id, performance_metric, created_date)
+SELECT 
+    e.exercise_id,
+    u.user_id,
+    28,
+    '2023-05-10'
+FROM
+    public.exercise e, public.user u
+WHERE
+    e.exercise = 'Barbell Squats' AND u.first_name = 'Benyamin';
+
+INSERT INTO public.exercise_tracking (exercise_id, user_id, performance_metric, created_date)
+SELECT 
+    e.exercise_id,
+    u.user_id,
+    33,
+    '2023-05-10'
+FROM
+    public.exercise e, public.user u
+WHERE
+    e.exercise = 'Barbell Squats' AND u.first_name = 'Yusuf';
+
+INSERT INTO public.exercise_tracking (exercise_id, user_id, performance_metric, created_date)
+SELECT 
+    e.exercise_id,
+    u.user_id,
+    18,
+    '2023-05-10'
+FROM
+    public.exercise e, public.user u
+WHERE
+    e.exercise = 'Barbell Squats' AND u.first_name = 'Imran';
+
+INSERT INTO public.exercise_tracking (exercise_id, user_id, performance_metric, created_date)
+SELECT 
+    e.exercise_id,
+    u.user_id,
+    38,
+    '2023-05-25'
+FROM
+    public.exercise e, public.user u
+WHERE
+    e.exercise = 'Barbell Squats' AND u.first_name = 'Benyamin';
+
+INSERT INTO public.exercise_tracking (exercise_id, user_id, performance_metric, created_date)
+SELECT 
+    e.exercise_id,
+    u.user_id,
+    40.5,
+    '2023-06-08'
+FROM
+    public.exercise e, public.user u
+WHERE
+    e.exercise = 'Barbell Squats' AND u.first_name = 'Benyamin';
+
---
+++ database/initial-migration/leg-press.sql
@@ -0,0 +1,648 @@
+INSERT INTO public.exercise_tracking (exercise_id, user_id, performance_metric, created_date)
+SELECT 
+    e.exercise_id,
+    u.user_id,
+    70,
+    '2023-06-22'
+FROM
+    public.exercise e, public.user u
+WHERE
+    e.exercise = 'Leg Press' AND u.first_name = 'Benyamin';
+
+INSERT INTO public.exercise_tracking (exercise_id, user_id, performance_metric, created_date)
+SELECT 
+    e.exercise_id,
+    u.user_id,
+    70,
+    '2023-06-22'
+FROM
+    public.exercise e, public.user u
+WHERE
+    e.exercise = 'Leg Press' AND u.first_name = 'Imran';
+
+INSERT INTO public.exercise_tracking (exercise_id, user_id, performance_metric, created_date)
+SELECT 
+    e.exercise_id,
+    u.user_id,
+    70,
+    '2023-07-07'
+FROM
+    public.exercise e, public.user u
+WHERE
+    e.exercise = 'Leg Press' AND u.first_name = 'Benyamin';
+
+INSERT INTO public.exercise_tracking (exercise_id, user_id, performance_metric, created_date)
+SELECT 
+    e.exercise_id,
+    u.user_id,
+    120,
+    '2023-07-07'
+FROM
+    public.exercise e, public.user u
+WHERE
+    e.exercise = 'Leg Press' AND u.first_name = 'Yusuf';
+
+INSERT INTO public.exercise_tracking (exercise_id, user_id, performance_metric, created_date)
+SELECT 
+    e.exercise_id,
+    u.user_id,
+    70,
+    '2023-07-07'
+FROM
+    public.exercise e, public.user u
+WHERE
+    e.exercise = 'Leg Press' AND u.first_name = 'Imran';
+
+INSERT INTO public.exercise_tracking (exercise_id, user_id, performance_metric, created_date)
+SELECT 
+    e.exercise_id,
+    u.user_id,
+    70,
+    '2023-07-21'
+FROM
+    public.exercise e, public.user u
+WHERE
+    e.exercise = 'Leg Press' AND u.first_name = 'Benyamin';
+
+INSERT INTO public.exercise_tracking (exercise_id, user_id, performance_metric, created_date)
+SELECT 
+    e.exercise_id,
+    u.user_id,
+    120,
+    '2023-07-21'
+FROM
+    public.exercise e, public.user u
+WHERE
+    e.exercise = 'Leg Press' AND u.first_name = 'Yusuf';
+
+INSERT INTO public.exercise_tracking (exercise_id, user_id, performance_metric, created_date)
+SELECT 
+    e.exercise_id,
+    u.user_id,
+    70,
+    '2023-07-21'
+FROM
+    public.exercise e, public.user u
+WHERE
+    e.exercise = 'Leg Press' AND u.first_name = 'Imran';
+
+INSERT INTO public.exercise_tracking (exercise_id, user_id, performance_metric, created_date)
+SELECT 
+    e.exercise_id,
+    u.user_id,
+    70,
+    '2023-08-05'
+FROM
+    public.exercise e, public.user u
+WHERE
+    e.exercise = 'Leg Press' AND u.first_name = 'Benyamin';
+
+INSERT INTO public.exercise_tracking (exercise_id, user_id, performance_metric, created_date)
+SELECT 
+    e.exercise_id,
+    u.user_id,
+    120,
+    '2023-08-05'
+FROM
+    public.exercise e, public.user u
+WHERE
+    e.exercise = 'Leg Press' AND u.first_name = 'Yusuf';
+
+INSERT INTO public.exercise_tracking (exercise_id, user_id, performance_metric, created_date)
+SELECT 
+    e.exercise_id,
+    u.user_id,
+    70,
+    '2023-08-05'
+FROM
+    public.exercise e, public.user u
+WHERE
+    e.exercise = 'Leg Press' AND u.first_name = 'Imran';
+
+INSERT INTO public.exercise_tracking (exercise_id, user_id, performance_metric, created_date)
+SELECT 
+    e.exercise_id,
+    u.user_id,
+    90,
+    '2023-08-19'
+FROM
+    public.exercise e, public.user u
+WHERE
+    e.exercise = 'Leg Press' AND u.first_name = 'Benyamin';
+
+INSERT INTO public.exercise_tracking (exercise_id, user_id, performance_metric, created_date)
+SELECT 
+    e.exercise_id,
+    u.user_id,
+    130,
+    '2023-08-19'
+FROM
+    public.exercise e, public.user u
+WHERE
+    e.exercise = 'Leg Press' AND u.first_name = 'Yusuf';
+
+INSERT INTO public.exercise_tracking (exercise_id, user_id, performance_metric, created_date)
+SELECT 
+    e.exercise_id,
+    u.user_id,
+    110,
+    '2023-08-19'
+FROM
+    public.exercise e, public.user u
+WHERE
+    e.exercise = 'Leg Press' AND u.first_name = 'Imran';
+
+INSERT INTO public.exercise_tracking (exercise_id, user_id, performance_metric, created_date)
+SELECT 
+    e.exercise_id,
+    u.user_id,
+    90,
+    '2023-09-02'
+FROM
+    public.exercise e, public.user u
+WHERE
+    e.exercise = 'Leg Press' AND u.first_name = 'Benyamin';
+
+INSERT INTO public.exercise_tracking (exercise_id, user_id, performance_metric, created_date)
+SELECT 
+    e.exercise_id,
+    u.user_id,
+    130,
+    '2023-09-02'
+FROM
+    public.exercise e, public.user u
+WHERE
+    e.exercise = 'Leg Press' AND u.first_name = 'Yusuf';
+
+INSERT INTO public.exercise_tracking (exercise_id, user_id, performance_metric, created_date)
+SELECT 
+    e.exercise_id,
+    u.user_id,
+    100,
+    '2023-09-17'
+FROM
+    public.exercise e, public.user u
+WHERE
+    e.exercise = 'Leg Press' AND u.first_name = 'Benyamin';
+
+INSERT INTO public.exercise_tracking (exercise_id, user_id, performance_metric, created_date)
+SELECT 
+    e.exercise_id,
+    u.user_id,
+    130,
+    '2023-09-17'
+FROM
+    public.exercise e, public.user u
+WHERE
+    e.exercise = 'Leg Press' AND u.first_name = 'Yusuf';
+
+INSERT INTO public.exercise_tracking (exercise_id, user_id, performance_metric, created_date)
+SELECT 
+    e.exercise_id,
+    u.user_id,
+    50,
+    '2023-09-17'
+FROM
+    public.exercise e, public.user u
+WHERE
+    e.exercise = 'Leg Press' AND u.first_name = 'Imran';
+
+INSERT INTO public.exercise_tracking (exercise_id, user_id, performance_metric, created_date)
+SELECT 
+    e.exercise_id,
+    u.user_id,
+    100,
+    '2023-10-01'
+FROM
+    public.exercise e, public.user u
+WHERE
+    e.exercise = 'Leg Press' AND u.first_name = 'Benyamin';
+
+INSERT INTO public.exercise_tracking (exercise_id, user_id, performance_metric, created_date)
+SELECT 
+    e.exercise_id,
+    u.user_id,
+    130,
+    '2023-10-01'
+FROM
+    public.exercise e, public.user u
+WHERE
+    e.exercise = 'Leg Press' AND u.first_name = 'Yusuf';
+
+INSERT INTO public.exercise_tracking (exercise_id, user_id, performance_metric, created_date)
+SELECT 
+    e.exercise_id,
+    u.user_id,
+    100,
+    '2023-10-01'
+FROM
+    public.exercise e, public.user u
+WHERE
+    e.exercise = 'Leg Press' AND u.first_name = 'Imran';
+
+INSERT INTO public.exercise_tracking (exercise_id, user_id, performance_metric, created_date)
+SELECT 
+    e.exercise_id,
+    u.user_id,
+    110,
+    '2023-10-16'
+FROM
+    public.exercise e, public.user u
+WHERE
+    e.exercise = 'Leg Press' AND u.first_name = 'Benyamin';
+
+INSERT INTO public.exercise_tracking (exercise_id, user_id, performance_metric, created_date)
+SELECT 
+    e.exercise_id,
+    u.user_id,
+    130,
+    '2023-10-16'
+FROM
+    public.exercise e, public.user u
+WHERE
+    e.exercise = 'Leg Press' AND u.first_name = 'Yusuf';
+
+INSERT INTO public.exercise_tracking (exercise_id, user_id, performance_metric, created_date)
+SELECT 
+    e.exercise_id,
+    u.user_id,
+    110,
+    '2023-10-30'
+FROM
+    public.exercise e, public.user u
+WHERE
+    e.exercise = 'Leg Press' AND u.first_name = 'Benyamin';
+
+INSERT INTO public.exercise_tracking (exercise_id, user_id, performance_metric, created_date)
+SELECT 
+    e.exercise_id,
+    u.user_id,
+    140,
+    '2023-10-30'
+FROM
+    public.exercise e, public.user u
+WHERE
+    e.exercise = 'Leg Press' AND u.first_name = 'Yusuf';
+
+INSERT INTO public.exercise_tracking (exercise_id, user_id, performance_metric, created_date)
+SELECT 
+    e.exercise_id,
+    u.user_id,
+    30,
+    '2023-10-30'
+FROM
+    public.exercise e, public.user u
+WHERE
+    e.exercise = 'Leg Press' AND u.first_name = 'Imran';
+
+INSERT INTO public.exercise_tracking (exercise_id, user_id, performance_metric, created_date)
+SELECT 
+    e.exercise_id,
+    u.user_id,
+    110,
+    '2023-11-13'
+FROM
+    public.exercise e, public.user u
+WHERE
+    e.exercise = 'Leg Press' AND u.first_name = 'Benyamin';
+
+INSERT INTO public.exercise_tracking (exercise_id, user_id, performance_metric, created_date)
+SELECT 
+    e.exercise_id,
+    u.user_id,
+    140,
+    '2023-11-13'
+FROM
+    public.exercise e, public.user u
+WHERE
+    e.exercise = 'Leg Press' AND u.first_name = 'Yusuf';
+
+INSERT INTO public.exercise_tracking (exercise_id, user_id, performance_metric, created_date)
+SELECT 
+    e.exercise_id,
+    u.user_id,
+    70,
+    '2023-11-13'
+FROM
+    public.exercise e, public.user u
+WHERE
+    e.exercise = 'Leg Press' AND u.first_name = 'Imran';
+
+INSERT INTO public.exercise_tracking (exercise_id, user_id, performance_metric, created_date)
+SELECT 
+    e.exercise_id,
+    u.user_id,
+    110,
+    '2023-11-28'
+FROM
+    public.exercise e, public.user u
+WHERE
+    e.exercise = 'Leg Press' AND u.first_name = 'Benyamin';
+
+INSERT INTO public.exercise_tracking (exercise_id, user_id, performance_metric, created_date)
+SELECT 
+    e.exercise_id,
+    u.user_id,
+    140,
+    '2023-11-28'
+FROM
+    public.exercise e, public.user u
+WHERE
+    e.exercise = 'Leg Press' AND u.first_name = 'Yusuf';
+
+INSERT INTO public.exercise_tracking (exercise_id, user_id, performance_metric, created_date)
+SELECT 
+    e.exercise_id,
+    u.user_id,
+    20,
+    '2023-11-28'
+FROM
+    public.exercise e, public.user u
+WHERE
+    e.exercise = 'Leg Press' AND u.first_name = 'Imran';
+
+INSERT INTO public.exercise_tracking (exercise_id, user_id, performance_metric, created_date)
+SELECT 
+    e.exercise_id,
+    u.user_id,
+    110,
+    '2023-12-12'
+FROM
+    public.exercise e, public.user u
+WHERE
+    e.exercise = 'Leg Press' AND u.first_name = 'Benyamin';
+
+INSERT INTO public.exercise_tracking (exercise_id, user_id, performance_metric, created_date)
+SELECT 
+    e.exercise_id,
+    u.user_id,
+    140,
+    '2023-12-12'
+FROM
+    public.exercise e, public.user u
+WHERE
+    e.exercise = 'Leg Press' AND u.first_name = 'Yusuf';
+
+INSERT INTO public.exercise_tracking (exercise_id, user_id, performance_metric, created_date)
+SELECT 
+    e.exercise_id,
+    u.user_id,
+    120,
+    '2023-12-27'
+FROM
+    public.exercise e, public.user u
+WHERE
+    e.exercise = 'Leg Press' AND u.first_name = 'Benyamin';
+
+INSERT INTO public.exercise_tracking (exercise_id, user_id, performance_metric, created_date)
+SELECT 
+    e.exercise_id,
+    u.user_id,
+    140,
+    '2023-12-27'
+FROM
+    public.exercise e, public.user u
+WHERE
+    e.exercise = 'Leg Press' AND u.first_name = 'Yusuf';
+
+INSERT INTO public.exercise_tracking (exercise_id, user_id, performance_metric, created_date)
+SELECT 
+    e.exercise_id,
+    u.user_id,
+    120,
+    '2024-01-10'
+FROM
+    public.exercise e, public.user u
+WHERE
+    e.exercise = 'Leg Press' AND u.first_name = 'Benyamin';
+
+INSERT INTO public.exercise_tracking (exercise_id, user_id, performance_metric, created_date)
+SELECT 
+    e.exercise_id,
+    u.user_id,
+    140,
+    '2024-01-10'
+FROM
+    public.exercise e, public.user u
+WHERE
+    e.exercise = 'Leg Press' AND u.first_name = 'Yusuf';
+
+INSERT INTO public.exercise_tracking (exercise_id, user_id, performance_metric, created_date)
+SELECT 
+    e.exercise_id,
+    u.user_id,
+    120,
+    '2024-01-24'
+FROM
+    public.exercise e, public.user u
+WHERE
+    e.exercise = 'Leg Press' AND u.first_name = 'Benyamin';
+
+INSERT INTO public.exercise_tracking (exercise_id, user_id, performance_metric, created_date)
+SELECT 
+    e.exercise_id,
+    u.user_id,
+    140,
+    '2024-01-24'
+FROM
+    public.exercise e, public.user u
+WHERE
+    e.exercise = 'Leg Press' AND u.first_name = 'Yusuf';
+
+INSERT INTO public.exercise_tracking (exercise_id, user_id, performance_metric, created_date)
+SELECT 
+    e.exercise_id,
+    u.user_id,
+    120,
+    '2024-02-03'
+FROM
+    public.exercise e, public.user u
+WHERE
+    e.exercise = 'Leg Press' AND u.first_name = 'Benyamin';
+
+INSERT INTO public.exercise_tracking (exercise_id, user_id, performance_metric, created_date)
+SELECT 
+    e.exercise_id,
+    u.user_id,
+    120,
+    '2024-02-03'
+FROM
+    public.exercise e, public.user u
+WHERE
+    e.exercise = 'Leg Press' AND u.first_name = 'Imran';
+
+INSERT INTO public.exercise_tracking (exercise_id, user_id, performance_metric, created_date)
+SELECT 
+    e.exercise_id,
+    u.user_id,
+    120,
+    '2024-02-04'
+FROM
+    public.exercise e, public.user u
+WHERE
+    e.exercise = 'Leg Press' AND u.first_name = 'Benyamin';
+
+INSERT INTO public.exercise_tracking (exercise_id, user_id, performance_metric, created_date)
+SELECT 
+    e.exercise_id,
+    u.user_id,
+    150,
+    '2024-02-04'
+FROM
+    public.exercise e, public.user u
+WHERE
+    e.exercise = 'Leg Press' AND u.first_name = 'Yusuf';
+
+INSERT INTO public.exercise_tracking (exercise_id, user_id, performance_metric, created_date)
+SELECT 
+    e.exercise_id,
+    u.user_id,
+    120,
+    '2024-02-05'
+FROM
+    public.exercise e, public.user u
+WHERE
+    e.exercise = 'Leg Press' AND u.first_name = 'Benyamin';
+
+INSERT INTO public.exercise_tracking (exercise_id, user_id, performance_metric, created_date)
+SELECT 
+    e.exercise_id,
+    u.user_id,
+    160,
+    '2024-02-05'
+FROM
+    public.exercise e, public.user u
+WHERE
+    e.exercise = 'Leg Press' AND u.first_name = 'Yusuf';
+
+INSERT INTO public.exercise_tracking (exercise_id, user_id, performance_metric, created_date)
+SELECT 
+    e.exercise_id,
+    u.user_id,
+    120,
+    '2024-02-06'
+FROM
+    public.exercise e, public.user u
+WHERE
+    e.exercise = 'Leg Press' AND u.first_name = 'Benyamin';
+
+INSERT INTO public.exercise_tracking (exercise_id, user_id, performance_metric, created_date)
+SELECT 
+    e.exercise_id,
+    u.user_id,
+    150,
+    '2024-02-06'
+FROM
+    public.exercise e, public.user u
+WHERE
+    e.exercise = 'Leg Press' AND u.first_name = 'Yusuf';
+
+INSERT INTO public.exercise_tracking (exercise_id, user_id, performance_metric, created_date)
+SELECT 
+    e.exercise_id,
+    u.user_id,
+    120,
+    '2024-02-07'
+FROM
+    public.exercise e, public.user u
+WHERE
+    e.exercise = 'Leg Press' AND u.first_name = 'Benyamin';
+
+INSERT INTO public.exercise_tracking (exercise_id, user_id, performance_metric, created_date)
+SELECT 
+    e.exercise_id,
+    u.user_id,
+    100,
+    '2024-02-07'
+FROM
+    public.exercise e, public.user u
+WHERE
+    e.exercise = 'Leg Press' AND u.first_name = 'Imran';
+
+INSERT INTO public.exercise_tracking (exercise_id, user_id, performance_metric, created_date)
+SELECT 
+    e.exercise_id,
+    u.user_id,
+    120,
+    '2024-02-08'
+FROM
+    public.exercise e, public.user u
+WHERE
+    e.exercise = 'Leg Press' AND u.first_name = 'Benyamin';
+
+INSERT INTO public.exercise_tracking (exercise_id, user_id, performance_metric, created_date)
+SELECT 
+    e.exercise_id,
+    u.user_id,
+    150,
+    '2024-02-08'
+FROM
+    public.exercise e, public.user u
+WHERE
+    e.exercise = 'Leg Press' AND u.first_name = 'Yusuf';
+
+INSERT INTO public.exercise_tracking (exercise_id, user_id, performance_metric, created_date)
+SELECT 
+    e.exercise_id,
+    u.user_id,
+    100,
+    '2024-02-08'
+FROM
+    public.exercise e, public.user u
+WHERE
+    e.exercise = 'Leg Press' AND u.first_name = 'Imran';
+
+INSERT INTO public.exercise_tracking (exercise_id, user_id, performance_metric, created_date)
+SELECT 
+    e.exercise_id,
+    u.user_id,
+    120,
+    '2024-02-10'
+FROM
+    public.exercise e, public.user u
+WHERE
+    e.exercise = 'Leg Press' AND u.first_name = 'Benyamin';
+
+INSERT INTO public.exercise_tracking (exercise_id, user_id, performance_metric, created_date)
+SELECT 
+    e.exercise_id,
+    u.user_id,
+    150,
+    '2024-02-10'
+FROM
+    public.exercise e, public.user u
+WHERE
+    e.exercise = 'Leg Press' AND u.first_name = 'Yusuf';
+
+INSERT INTO public.exercise_tracking (exercise_id, user_id, performance_metric, created_date)
+SELECT 
+    e.exercise_id,
+    u.user_id,
+    130,
+    '2024-02-18'
+FROM
+    public.exercise e, public.user u
+WHERE
+    e.exercise = 'Leg Press' AND u.first_name = 'Benyamin';
+
+INSERT INTO public.exercise_tracking (exercise_id, user_id, performance_metric, created_date)
+SELECT 
+    e.exercise_id,
+    u.user_id,
+    160,
+    '2024-02-18'
+FROM
+    public.exercise e, public.user u
+WHERE
+    e.exercise = 'Leg Press' AND u.first_name = 'Yusuf';
+
+INSERT INTO public.exercise_tracking (exercise_id, user_id, performance_metric, created_date)
+SELECT 
+    e.exercise_id,
+    u.user_id,
+    90,
+    '2024-02-18'
+FROM
+    public.exercise e, public.user u
+WHERE
+    e.exercise = 'Leg Press' AND u.first_name = 'Imran';
